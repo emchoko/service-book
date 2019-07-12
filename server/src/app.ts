8d592a4608072ac21a57d060f8bbcb1cd2b61564
Emil Lozev <emilfrogy@gmail.com>
2019-07-12 15:05:19 +0100

    added sequelize

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 import express from 'express';
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,32 +7,25 @@ const app: express.Application = express();
 
 const PORT: number = 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function (_, _1, next) {
+  next(createError(404, 'Resource not found!'));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ type: 'error', message: res.locals.message });
 });
 
 app.listen(PORT, () => {
