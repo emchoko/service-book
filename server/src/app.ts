918f52e942e15ec32513734746727516ec0be71b
Emil Lozev <emilfrogy@gmail.com>
2019-07-12 14:29:52 +0100

    setup of the server project finished

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,13 @@
 var createError = require('http-errors');
-var express = require('express');
+import express from 'express';
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const PORT = 3000;
-
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app: express.Application = express();
 
-var app = express();
+const PORT: number = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,15 +20,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,7 +38,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Application listening on port: ${PORT}`);
 })
 
 module.exports = app;
