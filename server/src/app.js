dd93cddea89dd19d4237e2ec4d4f4216c49070e7
Emil Lozev <emilfrogy@gmail.com>
2019-07-12 16:09:04 +0100

    deleted some files

--- server/src/app.js
+++ server/src/app.js
@@ -1,40 +1,40 @@
-var createError = require('http-errors');
+let createError = require('http-errors');
 const express = require('express');
-var logger = require('morgan');
+let logger = require('morgan');
 require('dotenv').config();
 const db = require('./config/db');
 
-var indexRouter = require('./routes/index');
+let indexRouter = require('./routes/index');
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (_, _1, next) {
+app.use(function(_, _1, next) {
   next(createError(404, 'Resource not found!'));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({ type: 'error', message: res.locals.message });
+  res.json({type: 'error', message: res.locals.message});
 });
 
 
 db.connection.sync()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Application listening on port: ${PORT}`);
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Application listening on port: ${PORT}`);
+      });
     });
-  });
 module.exports = app;
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,23 +2,23 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD, {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    define: {
-        timestamps: false
-    }
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
+  define: {
+    timestamps: false,
+  },
 });
 
 sequelize
     .authenticate()
     .then(() => {
-        console.log('Connection was successful');
+      console.log('Connection was successful');
     })
     .catch((err) => {
-        console.log('Unable to establish connection!', err.message);
+      console.log('Unable to establish connection!', err.message);
     });
 
-let db = {};
+const db = {};
 db.connection = sequelize;
 db.types = Sequelize;
 
