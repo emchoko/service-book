8248c1974ade7947605de957b1c6152422de6eed
Emil Lozev <emilfrogy@gmail.com>
2019-07-16 14:57:11 +0100

    checking body is valid

--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -1,17 +1,18 @@
-module.exports = {
-  createClient: (req, res) => {
-    if (!req.body.email) {
-      return res.json({message: 'An error occured: Email not provided'});
-    }
+module.exports = (db, router) => {
+  var createClient = (req, res) => {
+    if (!req.body) return res.status(412).json({ message: 'Empty body' });
+    if (!req.body.email) return res.status(412).json({ message: 'An error occured: Email not provided' });
 
+    // async.waterfall()
     db.clients.create(req.body)
-        .then((createdObj) => {
-          res.json(createdObj);
-        })
-        .catch((err) => {
-          res.status(500).json({'message': 'error'});
-        });
+      .then((createdObj) => {
+        res.json(createdObj);
+      })
+      .catch((err) => {
+        res.status(500).json({ 'message': 'error' });
+      });
 
     return res.json(req.body);
-  },
+  };
+  router.post('/client', createClient);
 };
