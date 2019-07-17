ef89bda357c08d29907595963b5163fbc70d8374
Emil Lozev <emilfrogy@gmail.com>
2019-07-17 12:29:19 +0100

    tests pass

--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -1,3 +1,5 @@
+const async = require('async');
+
 module.exports = (db, app) => {
   var createClient = (req, res) => {
     if (!req.body) return res.status(412).json({ message: 'Empty body' });
@@ -7,36 +9,39 @@ module.exports = (db, app) => {
 
       // find if the client already exists
       done => {
-        db.clients.findOne({where: {email: req.body.email}})
-        .then(client => {
-          if(client !== null) {
-            let error = new Error('Client with this email already exists');
-            error.status = 412;
-            done(error);
-          }
-        });
+        db.clients.findOne({ where: { email: req.body.email } })
+          .then(client => {
+            if (client !== null) {
+              res.status(412).json({message: 'User with this email already exists'});
+              return done();
+            }
+            return done();
+          })
+          .catch(dbError => {
+            return done(dbError);
+          });
       },
-      
+
       // create the client
       (done) => {
         db.clients.create(req.body)
-        .then(client => {
-          res.status(200).json(client);
-        })
-        .catch(err => {
-          done(err);
-        });
+          .then(client => {
+            res.status(200).json(client);
+            return done();
+          })
+          .catch(err => {
+            return done(err);
+          });
       }
+
     ],
       // error handling
       function (error) {
         if (error) {
-          console.log(`status: ${error.status}`);
-          res.status(error.status).json({ message: 'An error occured: ' + error.message });
+          return res.status(500).json(error);
         }
+        res.end();
       });
-
-    return res.json(req.body);
   };
 
   app.post('/client', createClient);
