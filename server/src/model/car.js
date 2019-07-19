c5acec31901c7322cb4c69a02d8932d690677bc8
Emil Lozev <emilfrogy@gmail.com>
2019-07-19 16:33:55 +0100

    car created

---
+++ server/src/model/car.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (connection, DataTypes) => {
+    const Car = connection.define('client_car', {
+        license_plate: {
+            primaryKey: true,
+            type: DataTypes.STRING(20),
+        },
+        power_in_hp: {
+            type: DataTypes.INTEGER
+        },
+        is_filter_particles: {
+            type: DataTypes.BOOLEAN
+        },
+        engine_code: {
+            type: DataTypes.STRING(30)
+        }
+    });
+    return Car;
+};
