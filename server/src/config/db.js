728f605576ab47e2f51ef07024f81444b44619d9
Emil Lozev <emilfrogy@gmail.com>
2019-07-19 16:40:00 +0100

    installed nodemon

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,28 +2,31 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const connection = new Sequelize(
-    process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD, {
-      host: process.env.HOST,
-      dialect: process.env.DIALECT,
-      define: {
-        timestamps: false,
-      },
-    });
+  process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    define: {
+      timestamps: false,
+    },
+  });
 
 connection
-    .authenticate()
-    .then(() => {
-      console.log('Connection was successful');
-    })
-    .catch((err) => {
-      console.log('Unable to establish connection!', err.message);
-    });
+  .authenticate()
+  .then(() => {
+    console.log('Connection was successful');
+  })
+  .catch((err) => {
+    console.log('Unable to establish connection!', err.message);
+  });
 
 const db = {};
 db.connection = connection;
 db.DataTypes = Sequelize;
 
 db.clients = require('../model/client')(connection, Sequelize);
-db.
+db.clientCars = require('../model/client_car')(connection, Sequelize);
+
+db.clients.hasMany(db.clientCars);
+db.clientCars.belongsTo(db.client, { as: 'car' });
 
 module.exports = db;
