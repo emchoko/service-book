28101f5905269dea3b700456d62de023ed0acd3c
Emil Lozev <emilfrogy@gmail.com>
2019-07-21 11:17:14 +0100

    test suite for car and removed redundant var

---
+++ server/tests/routes/client_car.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-http'));
+const app = require('../../src/app');
+const request = chai.request;
+
+describe('Car Route Suite', () => {
+    describe('/client/:id/car POST', () => {
+        it('');
+    });
+});
