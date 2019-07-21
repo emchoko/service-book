7515c5aec3a61fcc63e8fec9fadb22eef4a86483
Emil Lozev <emilfrogy@gmail.com>
2019-07-21 11:52:14 +0100

    test suites for /client/:id/car

--- server/tests/routes/client_car.test.js
+++ server/tests/routes/client_car.test.js
@@ -5,26 +5,62 @@ const app = require('../../src/app');
 const request = chai.request;
 
 describe('Car Route Suite', () => {
+  beforeEach(() => {
+    carObj = { license_plate: 'CA3124KT', make: 'Peugeot', model: '204', year: 2019 };
+  });
+
   describe('/client/:id/car POST', () => {
     it('should request with empty body fail', function (done) {
       request(app)
-
+        .post('/client/1/car')
+        .send({})
+        .end(function (err, res) {
+          expect(res).to.have.status(412);
+          done();
+        });
     });
 
     it('should request with non existant client ID fail', function (done) {
-
+      request(app)
+        .post('/client/312312/car')
+        .send(carObj)
+        .end(function (err, res) {
+          expect(res).to.have.status(404);
+          done();
+        });
     });
 
     it('should request without license plate fail', function (done) {
-
+      carObj.license_plate = '';
+      request(app)
+        .post('/client/1/car')
+        .send(carObj)
+        .end(function (err, res) {
+          expect(res).to.have.status(412);
+          done();
+        });
     });
 
-    it('should request without make, model and year fail', function (done) {
-
+    it('should request without make and model fail', function (done) {
+      carObj.make = '';
+      carObj.model = '';
+      request(app)
+        .post('/client/1/car')
+        .send(carObj)
+        .end(function (err, res) {
+          expect(res).to.have.status(412);
+          done();
+        });
     });
 
     it('should valid request pass', function (done) {
-
+      request(app)
+        .post('/client/1/car')
+        .send(carObj)
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
     });
   });
 });
