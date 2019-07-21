d1a7655b000004771de6a0265bba0f34c5da993b
Emil Lozev <emilfrogy@gmail.com>
2019-07-21 11:07:55 +0100

    updated register with phone or email

--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -3,15 +3,18 @@ const async = require('async');
 module.exports = (path, db, app) => {
   var createClient = (req, res) => {
     if (!req.body) return res.status(412).json({ message: 'Empty body' });
-    
+    if (!req.body.email && !req.body.telephone)
+      return res.status(412).json({ message: 'Either email or telephone should be provided' });
+
     async.waterfall([
 
       // find if the client already exists
       done => {
-        db.clients.findOne({ where: { email: req.body.email } })
+        const queryObj = !req.body.email ? { telephone: req.body.telephone } : { email: req.body.email };
+        db.clients.findOne({ where: queryObj })
           .then(client => {
             if (client !== null) {
-              res.status(412).json({message: 'User with this email already exists'});
+              res.status(412).json({ message: 'User with this email already exists' });
               return done();
             }
             return done();
