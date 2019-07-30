5d343f4cbee54756148d88de3c623a72cc72b9d4
Emil Lozev <emilfrogy@gmail.com>
2019-07-30 23:40:22 +0100

    wired add service scrreen

--- app/screens/AddServiceScreen.js
+++ app/screens/AddServiceScreen.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import { View, Text, Button } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { TextField } from 'react-native-material-textfield';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 import styles from './../constants/Styles';
+import { SegmentedControls } from 'react-native-radio-buttons';
 import Fetcher from '../utils/Fetcher';
 
+function formatTime(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const leftOverseconds = seconds % 60;
+  const minutes = (seconds - leftOverseconds) / 60;
+  return `${(minutes < 10) ? `0${minutes}` : `${minutes}`}:${(leftOverseconds < 10) ? `0${leftOverseconds}` : `${leftOverseconds}`}`;
+}
+
+function addServiceReducer(state, action) {
+  switch (action.type) {
+    case 'field':
+      return {
+        ...state,
+        [action.field_name]: action.value,
+      }
+    default:
+      break;
+  }
+}
+
+const kmOptions = [10, 15, 50, 60];
+
+const initialState = {
+  kilometers: '',
+  next_change_km: kmOptions[0],
+  length_of_service: 0,
+}
+
 export default function AddServiceScreen() {
+
+  const [state, dispatch] = useReducer(addServiceReducer, initialState);
+  const [time, setTime] = useState('');
+
+  const { kilometers, next_change_km } = state;
+
+  useEffect(() => {
+    initialTime = Date.now();
+
+    const timeInterval = setInterval(() => {
+      setTime(formatTime(Date.now() - initialTime));
+    }, 1000);
+
+    return () => clearInterval(timeInterval);
+  }, []);
+
+  const serviceCompleted = () => {
+    const intKm = parseInt(kilometers, 10);
+    const service = {
+      date: Date.now(),
+      kilometers: intKm,
+      next_change_km: intKm + next_change_km,
+      length_of_service: Date.now() - initialTime,
+    };
+    
+
+  }
 
   return (
     <View style={styles.container}>
@@ -20,12 +75,41 @@ export default function AddServiceScreen() {
         </View>
 
         <View style={styles.fieldsContainer}>
+          <Text style={styles.labelText}>Времетраене</Text>
+          <Text style={styles.timer}>{time}</Text>
 
           <TextField
             label='Текущи километри'
-            value='101000'
+            value={kilometers}
             keyboardType='numeric'
-            onChangeText={(text) => { }}
+            onChangeText={(text) => {
+              dispatch({
+                type: 'field',
+                value: text,
+                field_name: 'kilometers',
+              });
+            }}
+          />
+
+          <Text style={styles.labelText}>Следваща смяна след</Text>
+
+          <SegmentedControls
+            options={kmOptions}
+            onSelection={(option) => {
+              dispatch({
+                type: 'field',
+                value: option,
+                field_name: 'next_change_km'
+              })
+            }}
+            selectedOption={next_change_km}
+          />
+
+          <Button
+            title='Приключи'
+            color='#841584'
+            accessibilityLabel='Добави ново обслужване'
+            onPress={() => { serviceCompleted() }}
           />
 
         </View>
