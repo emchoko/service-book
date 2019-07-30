acf058e83d50438a6f71680123e31bb6aaa784ec
Emil Lozev <emilfrogy@gmail.com>
2019-07-30 14:24:50 +0100

    created car class

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,11 @@ const clientRoute = require('./client');
 const routes = [
   {
     path: '/client',
-    handler: require('./client')
+    handler: require('./client'),
+  },
+  {
+    path: '/car',
+    handler: require('./car'),
   }
 ]
 
