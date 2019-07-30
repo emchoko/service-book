a88e0e504be5309c1d7137340af3bf41fecb86a4
Emil Lozev <emilfrogy@gmail.com>
2019-07-31 00:11:05 +0100

    product model

---
+++ server/src/model/product.js
@@ -0,0 +1,24 @@
+module.exports = (connection, DataTypes) => {
+  const Product = connection.define('product', {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_type: {
+      type: DataTypes.STRING(100),
+      nullable: false,
+    },
+    code: {
+      type: DataTypes.STRING(200),
+      nullable: false,
+    },
+    brand: {
+      type: DataTypes.STRING(200)
+    },
+    fluid_amount: {
+      type: DataTypes.FLOAT,
+    }
+  });
+  return Product;
+}
