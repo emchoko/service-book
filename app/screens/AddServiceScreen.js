16030e4bb2aaea80678c2459299b57cbe0986c3c
Emil Lozev <emilfrogy@gmail.com>
2019-07-30 17:55:29 +0100

    fixed broken import

--- app/screens/AddServiceScreen.js
+++ app/screens/AddServiceScreen.js
@@ -2,33 +2,34 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { TextField } from 'react-native-material-textfield';
-import { KeyboardAwareScrollView } from 'react-native-check-box';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 import styles from './../constants/Styles';
 import Fetcher from '../utils/Fetcher';
 
 export default function AddServiceScreen() {
 
-
   return (
     <View style={styles.container}>
       <KeyboardAwareScrollView
-        contentContainerStyle={styles.contentContainer}>
-          
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>Обслужване</Text>
-          </View>
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
 
-          <View style={styles.fieldsContainer}>
-            
-            <TextField
-              label='Текущи километри'
-              value='101000'
-              keyboardType='numeric'
-              onChangeText={(text) => {}}
-            />
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>Обслужване</Text>
+        </View>
 
-          </View>
-        </KeyboardAwareScrollView>
+        <View style={styles.fieldsContainer}>
+
+          <TextField
+            label='Текущи километри'
+            value='101000'
+            keyboardType='numeric'
+            onChangeText={(text) => { }}
+          />
+
+        </View>
+      </KeyboardAwareScrollView>
     </View>
-    );
-}
+  );
+};
