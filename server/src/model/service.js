7d1790b783dc59571f2df302b1593291ab6b42fd
Emil Lozev <emilfrogy@gmail.com>
2019-07-30 14:40:12 +0100

    fixed indentation and added service model

---
+++ server/src/model/service.js
@@ -0,0 +1,23 @@
+module.exports = (connection, DataTypes) => {
+  const Service = connection.define('service', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+    kilometers: {
+      type: DataTypes.INTEGER,
+    },
+    next_change_km: {
+      type: DataTypes.INTEGER,
+    },
+    length_of_service: {
+      type: DataTypes.INTEGER,
+    }
+  });
+
+  return Service;
+}
