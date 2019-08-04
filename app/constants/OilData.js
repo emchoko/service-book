21245261b2ef6c83879ee8b876bfe1049e505ade
Emil Lozev <emilfrogy@gmail.com>
2019-08-04 22:38:43 +0100

    exported oil data to a separate file

---
+++ app/constants/OilData.js
@@ -0,0 +1,14 @@
+export default {
+  oil_brands: [
+    { value: 'Castrol' },
+    { value: 'Elf' },
+    { value: 'Mobil' }
+  ],
+  viscosities: [
+    { value: '5w40' },
+    { value: '10w40' },
+    { value: '10w50' }
+  ],
+  oil_change_options: [10, 15, 20],
+
+}
