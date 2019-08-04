1b18fe33e2c083d9590b9de24694413d82601f0c
Emil Lozev <emilfrogy@gmail.com>
2019-08-04 22:14:00 +0100

    created new association for many to many between product and services

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -30,7 +30,7 @@ db.clientCars = require('../model/client_car')(connection, Sequelize);
 db.internalCars = require('../model/internal_car')(connection, Sequelize);
 db.services = require('../model/service')(connection, Sequelize);
 db.products = require('../model/product')(connection, Sequelize);
-db.produc
+db.serviceProducts = require('../model/service_products')(connection, Sequelize);
 
 // Define associations
 db.clients.hasMany(db.clientCars);
@@ -42,7 +42,7 @@ db.clientCars.belongsTo(db.internalCars, { as: 'internalCar' });
 db.clientCars.hasMany(db.services);
 db.services.belongsTo(db.clientCars, { as: 'clientCar' });
 
-db.services.belongsToMany(db.products, {through: 'service_product'});
-db.products.belongsToMany(db.services, {through: 'service_product'});
+db.services.belongsToMany(db.products, {through: 'service_products', foreignKey: 'service_id'});
+db.products.belongsToMany(db.services, {through: 'service_products', foreignKey: 'product_id'});
 
 module.exports = db;
