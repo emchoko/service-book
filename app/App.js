20792d5ce26606fad8b1621c3bf882616e27a23c
Emil Lozev <emilfrogy@gmail.com>
2019-09-27 22:04:52 +0100

    removed ts error

--- app/App.js
+++ app/App.js
@@ -5,7 +5,6 @@ import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-import AppNavigator from './navigation/AppNavigator';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import StartServiceScreen from './screens/StartServiceScreen';
@@ -50,7 +49,7 @@ async function loadResourcesAsync() {
   ]);
 }
 
-function handleLoadingError(error: Error) {
+function handleLoadingError(error) {
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
   console.warn(error);
