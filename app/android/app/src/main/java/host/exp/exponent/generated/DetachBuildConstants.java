81e2f6247949492e8282b8fd1126ca12aed9b576
Emil Lozev <emilfrogy@gmail.com>
2019-09-27 21:58:27 +0100

    setup of application

--- app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp4858ef1f9ce3440d8de6b1f4290280fc://10.19.227.255:19000";
+  public static final String DEVELOPMENT_URL = "exp4858ef1f9ce3440d8de6b1f4290280fc://100.75.199.84:19000";
 
 }
