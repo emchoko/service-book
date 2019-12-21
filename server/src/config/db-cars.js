bcd0b89de65572aadda14aae1e03c46d21357ae3
emchoko <lozev.emil.05@gmail.com>
2019-12-21 17:54:11 +0200

    auto db done

---
+++ server/src/config/db-cars.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const connection = new Sequelize(
+  process.env.DATABASE_CAR, process.env.DB_USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    define: {
+      timestamps: false,
+      underscored: true,
+    },
+  });
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connection was successful to CarDB');
+  })
+  .catch((err) => {
+    console.log('Unable to establish connection to CarDB!', err.message);
+  });
+
+const db = {};
+db.connection = connection;
+db.DataTypes = Sequelize;
+
+module.exports = db;
