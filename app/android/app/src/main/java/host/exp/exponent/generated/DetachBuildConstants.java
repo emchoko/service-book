bc089873116a8dfb13acae8d39dd9e3ce5f9e9ad
emchoko <lozev.emil.05@gmail.com>
2019-12-22 15:19:02 +0200

    clear commit but started putting makes into the model

--- app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp4858ef1f9ce3440d8de6b1f4290280fc://192.168.0.105:19000";
+  public static final String DEVELOPMENT_URL = "exp4858ef1f9ce3440d8de6b1f4290280fc://192.168.100.10:19000";
 
 }
