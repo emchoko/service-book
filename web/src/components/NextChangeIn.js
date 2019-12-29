35b7fdf57d6ca314b8f6ffeef1c7cc9f98cd4986
emchoko <lozev.emil.05@gmail.com>
2019-12-29 18:23:22 +0200

    Segmented controls fixed

--- web/src/components/NextChangeIn.js
+++ web/src/components/NextChangeIn.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import formatJson from '../routes/AddCar';
-import { SegmentedControl } from 'segmented-control';
+// import { SegmentedControl } from 'segmented-control';
+import { SegmentedControl } from 'segmented-control-react';
+
 export function NextChangeIn(props) {
   return (<>
     <h4 className='mt-3'>Следваща смяна след км:</h4>
-    <SegmentedControl style={{ width: "100%", color: 'grey' }} // purple400
-      options={props.oil_change_options} setValue={(option) => {
+    <SegmentedControl
+      // style={{ width: "100%", color: 'grey' }} // purple400
+      segments={props.oil_change_options}
+      variant='dark'
+      onChangeSegment={(option) => {
         props.dispatch({
           type: 'field',
-          value: option,
+          value: props.oil_change_options[option].value,
           field_name: props.field_name
         });
       }} />
