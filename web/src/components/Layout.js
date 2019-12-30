c8dffabfe19ad68058b8ea45b4c2e4f8e6bae16e
emchoko <lozev.emil.05@gmail.com>
2019-12-30 11:35:13 +0200

    added progress bar

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import logo from '../images/logo-eloz.png';
+import { Link } from 'react-router-dom';
 
 export default ({ children, step }) => {
   const currentDate = new Date();
-
+  const percentage = step/4*100 + '%';
   return (
     <div className="wrapper">
       <header>
         <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
-          <img src={logo} className='img-fluid' width="90" height="90" alt="Лого" />
+          <Link to={'/'}>
+            <img src={logo} className='img-fluid' width="90" height="90" alt="Лого" />
+          </Link>
+
         </nav>
       </header>
 
-      <div className='container my-4' style={{ minHeight: '85vh' }}>
+      <div className='container my-4' style={{ minHeight: '80.5vh' }}>
+        <Link to={'/'}>
+          <button className='btn btn-danger'>Обратно към начало</button>
+        </Link>
         <h1>Стъпка {step} от 4</h1>
+        <div className="progress">
+          <div className="progress-bar progress-bar-striped progress-bar-animated" style={{width: percentage}}>
+            {percentage}
+          </div>
+        </div>
         <hr />
         {children}
       </div>
