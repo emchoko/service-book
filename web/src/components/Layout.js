81c1bd64411aa2cd8c403dee1f5faccc875e9446
emchoko <lozev.emil.05@gmail.com>
2020-01-04 10:28:44 +0000

    fixed vulnerabilities

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -11,22 +11,29 @@ export default ({ children, step }) => {
       <Helmet>
         <title>Сервизна история | Елоз - Смени Маслото</title>
         <meta name="description" content="Приложение за отчитане на сервизна история" />
-        <link rel="shortcut icon" type="image/png" href="http://www.smenimasloto.bg/wp-content/uploads/2018/05/cropped-eloz-ban-32x32.png"/>
+        <link rel="shortcut icon" type="image/png" href="http://www.smenimasloto.bg/wp-content/uploads/2018/05/cropped-eloz-ban-32x32.png" />
       </Helmet>
 
       <header>
         <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
-          <Link to={'/'}>
-            <img src={logo} className='img-fluid' width="90" height="90" alt="Лого" />
-          </Link>
-
+          <div className='row w-100 d-flex'>
+            <div className='col-sm-4 order-1 order-sm-0 d-flex justify-content-center align-items-center'>
+              <Link to={'/'}>
+                <button className='btn btn-danger mt-2 mt-sm-0'>Обратно към начало</button>
+              </Link>
+            </div>
+            <div className='col-sm-4 d-flex justify-content-center'>
+              <Link to={'/'}>
+                <img src={logo} className='img-fluid' width="90" height="90" alt="Лого" />
+              </Link>
+            </div>
+            <div className='col-sm-4'></div>
+          </div>
         </nav>
       </header>
 
       <div className='container my-4' style={{ minHeight: '80.5vh' }}>
-        <Link to={'/'}>
-          <button className='btn btn-danger'>Обратно към начало</button>
-        </Link>
+
         <h1>Стъпка {step} от 4</h1>
         <div className="progress">
           <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: percentage }}>
