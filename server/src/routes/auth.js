9301b327d71b28738d5282373a6c103e779f5f8f
emchoko <lozev.emil.05@gmail.com>
2020-01-05 15:35:36 +0000

    created middleware

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -49,16 +49,6 @@ module.exports = (path, db, app) => {
       })
   }
 
-  const verifyToken = (req, res) => {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided!' });
-
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token!' });
-
-      return res.status(200).send(decoded);
-    })
-  }
   app.post(path + '/register', registerUser);
   app.post(path + '/login', loginUser);
   app.get(path, verifyToken);
---
+++ server/src/utils/middleware.js
@@ -0,0 +1,13 @@
+const checkToken = (req, res, next) => {
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(401).json({ auth: false, message: 'No token provided!' });
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token!' });
+    req.decoded = decoded;
+    next();
+  })
+}
+module.exports = {
+  checkToken: checkToken,
+}
