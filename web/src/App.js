188165f02dc137ab03d1284b1e99553af316bb81
emchoko <lozev.emil.05@gmail.com>
2020-01-05 19:06:04 +0000

    created login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CookiesProvider } from 'react-cookie';
 
 import {
   BrowserRouter as Router,
@@ -9,30 +10,36 @@ import {
   Link
 } from "react-router-dom";
 
-import Home from './routes/Home';
+import StartService from './routes/StartService';
+import Login from './routes/Login';
 import AddClient from './routes/AddClient';
 import AddCar from './routes/AddCar';
-import AddService  from './routes/AddService';
+import AddService from './routes/AddService';
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/add-service">
-          <AddService />
-        </Route>
-        <Route path="/add-car">
-          <AddCar />
-        </Route>
-        <Route path="/add-client">
-          <AddClient />
-        </Route>
-        <Route path="/">
-          <Home />  
-        </Route>
-      </Switch>
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <Switch>
+          <Route path="/add-service">
+            <AddService />
+          </Route>
+          <Route path="/add-car">
+            <AddCar />
+          </Route>
+          <Route path="/add-client">
+            <AddClient />
+          </Route>
+          <Route path="/start-service">
+            <StartService />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </CookiesProvider>
   );
 }
 
--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../images/logo-eloz.png';
 import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
+import { useCookies } from 'react-cookie';
+
 
 export default ({ children, step }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['apiToken']);
+
+  useEffect(() => {
+
+    setCookie('apiToken', true, {
+      expires: new Date(Date.now() + 1000 * 20),
+    })
+  }, []);
   const currentDate = new Date();
   const percentage = step / 4 * 100 + '%';
+
   return (
     <div className="wrapper">
       <Helmet>
@@ -34,12 +45,16 @@ export default ({ children, step }) => {
 
       <div id='wrap' >
         <div id='main' className='container clear-top my-4'>
-          <p className='text-primary mb-0'>Стъпка {step} от 4</p>
-          <div className="progress">
-            <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: percentage }}>
-              {percentage}
+          {step && (
+            <div>
+              <p className='text-primary mb-0'>Стъпка {step} от 4</p>
+              <div className="progress">
+                <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: percentage }}>
+                  {percentage}
+                </div>
+              </div>
             </div>
-          </div>
+          )}
           <hr />
           <div>
             {children}
