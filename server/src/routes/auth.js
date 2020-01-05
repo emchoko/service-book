2a9a5759849bac09273abecc7639defb36c225d5
emchoko <lozev.emil.05@gmail.com>
2020-01-05 15:29:33 +0000

    login is possible

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -30,5 +30,36 @@ module.exports = (path, db, app) => {
     }
   }
 
+  const loginUser = (req, res) => {
+    db.users.findOne({ where: { username: req.body.username } })
+      .then((user) => {
+        if (!user) return res.status(404).json({ message: "User not found" });
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+
+        var token = jwt.sign({ id: user.id }, config.secret, {
+          expiresIn: 86400,
+        });
+
+        return res.status(200).send({ auth: true, token: token });
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  }
+
+  const verifyToken = (req, res) => {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided!' });
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token!' });
+
+      return res.status(200).send(decoded);
+    })
+  }
   app.post(path + '/register', registerUser);
+  app.post(path + '/login', loginUser);
+  app.get(path, verifyToken);
 }
