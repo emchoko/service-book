8462164d18eb668565e5936bd24c997065832ff1
emchoko <lozev.emil.05@gmail.com>
2020-01-09 00:12:14 +0000

    more padding on the service box

--- web/src/components/ServiceBox.js
+++ web/src/components/ServiceBox.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 export const ServiceBox = styled.div`
 margin-top: 1.4rem;
 margin-bottom: 1.4rem;
-padding: 1rem;
-border-radius: 2px;
+padding: 1.5rem;
+border-radius: 5px;
 box-shadow: 0px 8.71336px 58.089px rgba(0, 0, 0, 0.15);
 `;
