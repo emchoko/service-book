65bfde5267f485e9e70bd597f4a3936b4bd1c6d0
emchoko <lozev.emil.05@gmail.com>
2020-01-09 00:05:26 +0000

    add red label on next change

--- web/src/components/NextChangeIn.js
+++ web/src/components/NextChangeIn.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { SegmentedControl } from 'segmented-control-react';
 import Select from 'react-select';
 
 export function NextChangeIn(props) {
   return (<>
-    <h4 className='mt-3'>Следваща смяна след км:</h4>
+    <h4 className='mt-3'>Следваща смяна след км: <span className="text-danger">ЗАДЪЛЖИТЕЛНО</span></h4>
 
     <Select
       placeholder={'Избери следваща смяна след км'}
