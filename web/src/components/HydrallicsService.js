bf2565dc14c23cd4b4a8b4ee41d608328bef5460
emchoko <lozev.emil.05@gmail.com>
2020-01-09 00:10:14 +0000

    better looking service section

--- web/src/components/HydrallicsService.js
+++ web/src/components/HydrallicsService.js
@@ -2,17 +2,20 @@ import React from 'react';
 import OilData from '../constants/OilData';
 import { FluidFields } from './FluidFields';
 import { NextChangeIn } from './NextChangeIn';
+import { ServiceBox } from './ServiceBox';
 export function HydrallicsService(props) {
   return (<>
-    <h3 className='mt-3'>Хидравлика</h3>
-    <hr />
+    <ServiceBox>
+      <h3 className='mt-3'>Хидравлика</h3>
+      <hr />
 
-    <FluidFields dispatch={props.dispatch} img={require('../images/steering_wheel.png')} fluid_value={props.oil_hydraulics_amount} field_name={'oil_hydraulics_amount'} fluid_name={'oil_hydraulics'}
-      // oil data
-      brand_data={OilData.oil_hydraulics_brands} oil_brand={props.oil_hydraulics_brand} dropdown_field_name={'oil_hydraulics_brand'}
-      // viscosity data
-      viscosity_data={props.viscosity_variety} set_viscosity_variety={props.set_viscosity_variety} oil_viscosity={props.oil_hydraulics_viscosity} dropdown_field_name_viscosity={'oil_hydraulics_viscosity'} />
+      <FluidFields dispatch={props.dispatch} img={require('../images/steering_wheel.png')} fluid_value={props.oil_hydraulics_amount} field_name={'oil_hydraulics_amount'} fluid_name={'oil_hydraulics'}
+        // oil data
+        brand_data={OilData.oil_hydraulics_brands} oil_brand={props.oil_hydraulics_brand} dropdown_field_name={'oil_hydraulics_brand'}
+        // viscosity data
+        viscosity_data={props.viscosity_variety} set_viscosity_variety={props.set_viscosity_variety} oil_viscosity={props.oil_hydraulics_viscosity} dropdown_field_name_viscosity={'oil_hydraulics_viscosity'} />
 
-    <NextChangeIn dispatch={props.dispatch} oil_change_options={OilData.oil_hydraulics_change_options} next_change_km={props.next_hydraulics_change_km} field_name={'next_hydraulics_change_km'} />
+      <NextChangeIn dispatch={props.dispatch} oil_change_options={OilData.oil_hydraulics_change_options} next_change_km={props.next_hydraulics_change_km} field_name={'next_hydraulics_change_km'} />
+    </ServiceBox>
   </>);
 }
--- web/src/components/NotesArea.js
+++ web/src/components/NotesArea.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import { ServiceBox } from './ServiceBox';
 export function NotesArea(props) {
   return (<>
-    <h3 className='mt-3'>Забележки</h3>
-    <hr />
-    <div className='row'>
-      <div className='col-12'>
-        <div class="form-group">
-          {/* <label for="notes">Забележки по обслужването</label> */}
-          <textarea
-            className="form-control"
-            id="notes"
-            rows="10"
-            placeholder={'Въведи забележки по обслужването ...'}
-            value={props.notes}
-            onChange={(v) => {
-              props.dispatch({
-                type: 'field',
-                value: v.currentTarget.value,
-                field_name: 'notes',
-              });
-            }}
-          ></textarea>
+    <ServiceBox>
+      <h3 className='mt-3'>Забележки</h3>
+      <hr />
+      <div className='row'>
+        <div className='col-12'>
+          <div class="form-group">
+            {/* <label for="notes">Забележки по обслужването</label> */}
+            <textarea
+              className="form-control"
+              id="notes"
+              rows="10"
+              placeholder={'Въведи забележки по обслужването ...'}
+              value={props.notes}
+              onChange={(v) => {
+                props.dispatch({
+                  type: 'field',
+                  value: v.currentTarget.value,
+                  field_name: 'notes',
+                });
+              }}
+            ></textarea>
+          </div>
         </div>
       </div>
-    </div>
+    </ServiceBox>
   </>);
 }
