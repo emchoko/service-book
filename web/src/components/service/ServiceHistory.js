83cb54149a986a52719a46bb46af4facfa8695d8
emchoko <lozev.emil.05@gmail.com>
2020-03-13 02:00:18 +0000

    previous data can be accessed

--- web/src/components/service/ServiceHistory.js
+++ web/src/components/service/ServiceHistory.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ServiceBox } from '../ServiceBox';
 import { ServiceList } from '../ServiceList';
+import Fetcher from '../../utils/Fetcher';
 
 
-export default () => {
+export default ({ lp }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        Fetcher.GETservicesForCar(lp)
+            .then(res => {
+                res.json().then(body => {
+                    console.log(body);
+                    setServices(body);
+                });
+            }).catch(e => {
+                alert("Грешка с историята на автомобила!");
+                console.error(e);
+            });
+    }, []);
+
     return (
         <ServiceBox>
             <h2>История на обслужванията</h2>
 
-            <ServiceList services={[]} />
+            <ServiceList services={services} />
         </ServiceBox>
     )
 }
